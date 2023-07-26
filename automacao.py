287d0cc9784fe51e379864f70827af97ea0aad82
Jefferson Nascimento Souza <74577956+jefferson0710@users.noreply.github.com>
2023-07-26 16:21:40 -0300

    Add files via upload

---
+++ automacao.py
@@ -0,0 +1,44 @@
+import pyautogui
+import time
+
+pyautogui.alert('O codigo vai começar, não use nada do seu computador equanto o codigo carrega.')
+pyautogui.PAUSE = 1
+
+pyautogui.press('winleft')
+pyautogui.write('\\\snp305-003')
+pyautogui.press('enter')
+pyautogui.PAUSE = 1
+pyautogui.write('jefferson.nascimento')
+pyautogui.press('tab')
+pyautogui.write('G0710199912@')
+pyautogui.press('enter')
+
+pyautogui.write('soft')
+pyautogui.press('enter')
+pyautogui.write('programas padrão')
+pyautogui.press('enter')
+pyautogui.PAUSE = 1
+pyautogui.write('Oc')
+pyautogui.press('enter')
+
+pyautogui.write('Ocs Inventory')
+pyautogui.press('enter')
+pyautogui.keyDown('left')
+pyautogui.keyUp('left')
+pyautogui.press('enter')
+
+pyautogui.write('jefferson.nascimento')
+pyautogui.press('tab')
+pyautogui.write('G0710199912@')
+pyautogui.press('enter')
+
+pyautogui.press('enter')
+pyautogui.press('enter')
+pyautogui.press('enter')
+
+pyautogui.write('http://ocsnti/ocsinventory')
+pyautogui.press('enter')
+pyautogui.press('enter')
+
+pyautogui.click(x=1228, y=630)
+
